67d513d1db4f27aae28b25f337aa39c358b8bf60
Evgeny Bovykin <missingdays@etersoft.ru>
2015-09-23 13:35:44 +0300

    Improve installing

--- install.sh
+++ install.sh
@@ -7,6 +7,19 @@
 #
 
 mkdir ~/.handy-console
+
 git clone https://github.com/github/gitignore.git ~/.handy-console/ignores 
 mv ~/.handy-console/ignores/Global/* ~/.handy-console/ignores
 rm -rf ~./handy-console/ignores/Global
+
+mkdir ~/.handy-console/bin
+cp ./src/**/*.py ~/.handy-console/bin
+
+for f in ~/.handy-console/bin/*
+
+do
+    echo $f
+    ln -s $f ~/bin/"$(basename "$f" .py)"
+done
+
+
